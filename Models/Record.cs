be319b16a1ce81e3c32399b9ee30a5fd3e77953f
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-10-17 10:12:01 +1100

    Add Record model.

---
+++ Models/Record.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssessmentTask2Server.Models
+{
+    public class Record
+    {
+        public long Id { get; set; }
+        public string Heading { get; set; }
+        public string Description { get; set; }
+        public string Phone { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
