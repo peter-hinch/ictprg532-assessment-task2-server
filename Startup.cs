f44541a253f3745db0902fcb72bbec750636824a
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-10-17 09:54:49 +1100

    Add Swagger tools for documenting API.

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,13 @@ namespace AssessmentTask2Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Add Swagger
+            // Requires the use of Microsoft.OpenApi.Models .
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AssessmentTask2Server", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +42,13 @@ namespace AssessmentTask2Server
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                
+                // Add Swagger
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint(
+                    "/swagger/v1/swagger.json",
+                    "AssessmentTask2Server v1"
+                ));
             }
 
             app.UseHttpsRedirection();
