0122bef0c2113559421810447bd12f5dd5ae6def
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-10-17 10:18:05 +1100

    Add and configure DbContext for Record objects.

---
+++ DbContexts/DbContexts.cs
@@ -0,0 +1,19 @@
+﻿using AssessmentTask2Server.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssessmentTask2Server.DbContexts
+{
+    public class RecordDbContext : DbContext
+    {
+        public RecordDbContext(DbContextOptions<RecordDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Record> Records { get; set; }
+    }
+}
