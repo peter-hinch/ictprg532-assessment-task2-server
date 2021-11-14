55637052dd9d559dc950fc4a775e1451bd65089f
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-11-14 11:08:07 +1100

    Add sample data if database is empty.

--- Controllers/RecordsController.cs
+++ Controllers/RecordsController.cs
@@ -30,7 +30,137 @@ namespace AssessmentTask2Server
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Record>>> GetRecords()
         {
-            return await _context.Records.ToListAsync();
+            ActionResult<IEnumerable<Record>> records = await _context.Records.ToListAsync();
+
+            // Generate sample data if the database is empty.
+            if (_context.Records.ToList().Count() == 0)
+            {
+                _context.Records.Add(new Record {
+                    Heading = "Amon", Description = "Description 01", 
+                    Phone = "0123456789", Date = Convert.ToDateTime("01/01/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Dominik", Description = "Description 02", 
+                    Phone = "0456789123", Date = Convert.ToDateTime("02/02/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Marlowe", Description = "Description 03", 
+                    Phone = "0789123456", Date = Convert.ToDateTime("03/03/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Max", Description = "Description 04", 
+                    Phone = "0123456789", Date = Convert.ToDateTime("04/04/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Luke", Description = "Description 05", 
+                    Phone = "0456789123", Date = Convert.ToDateTime("05/05/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Jon", Description = "Description 06", 
+                    Phone = "0789123456", Date = Convert.ToDateTime("06/06/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Keith", Description = "Description 07", 
+                    Phone = "0123456789", Date = Convert.ToDateTime("07/07/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Zef", Description = "Description 08", 
+                    Phone = "0456789123", Date = Convert.ToDateTime("08/08/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Azeem", Description = "Description 09", 
+                    Phone = "0789123456", Date = Convert.ToDateTime("09/09/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Rob", Description = "Description 10", 
+                    Phone = "0123456789", Date = Convert.ToDateTime("10/10/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Steve", Description = "Description 11", 
+                    Phone = "0456789123", Date = Convert.ToDateTime("11/11/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Martyn", Description = "Description 12", 
+                    Phone = "0789123456", Date = Convert.ToDateTime("12/12/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Don", Description = "Description 13", 
+                    Phone = "0123456789", Date = Convert.ToDateTime("01/01/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Pablo", Description = "Description 14", 
+                    Phone = "0456789123", Date = Convert.ToDateTime("02/02/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Zackey", Description = "Description 15", 
+                    Phone = "0789123456", Date = Convert.ToDateTime("03/03/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Mike", Description = "Description 16", 
+                    Phone = "0123456789", Date = Convert.ToDateTime("04/04/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Edan", Description = "Description 17", 
+                    Phone = "0456789123", Date = Convert.ToDateTime("05/05/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Stacey", Description = "Description 18", 
+                    Phone = "0789123456", Date = Convert.ToDateTime("06/06/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Danny", Description = "Description 19", 
+                    Phone = "0123456789", Date = Convert.ToDateTime("07/07/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "David", Description = "Description 20", 
+                    Phone = "0456789123", Date = Convert.ToDateTime("08/08/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Jojo", Description = "Description 21", 
+                    Phone = "0789123456", Date = Convert.ToDateTime("09/09/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Jay", Description = "Description 22", 
+                    Phone = "0123456789", Date = Convert.ToDateTime("10/10/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Isaiah", Description = "Description 23", 
+                    Phone = "0456789123", Date = Convert.ToDateTime("11/11/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Hugh", Description = "Description 24", 
+                    Phone = "0789123456", Date = Convert.ToDateTime("12/12/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Talib", Description = "Description 25", 
+                    Phone = "0123456789", Date = Convert.ToDateTime("01/01/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Anderson", Description = "Description 26", 
+                    Phone = "0456789123", Date = Convert.ToDateTime("02/02/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Iggy", Description = "Description 27", 
+                    Phone = "0789123456", Date = Convert.ToDateTime("03/03/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Emily", Description = "Description 28", 
+                    Phone = "0123456789", Date = Convert.ToDateTime("04/04/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Kanye", Description = "Description 29", 
+                    Phone = "0456789123", Date = Convert.ToDateTime("05/05/2021")
+                });
+                _context.Records.Add(new Record {
+                    Heading = "Marlena", Description = "Description 30", 
+                    Phone = "0789123456", Date = Convert.ToDateTime("06/06/2021")
+                });
+                await _context.SaveChangesAsync();
+
+                records = await _context.Records.ToListAsync();
+            }
+
+            return records;
         }
 
         // GET: api/Records/5
